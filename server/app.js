cf52d3a1ed3299f6aeb4460ff6fdb630ecd3454c
AxAnder0095 <doriscafe@hotmail.com>
2026-02-26 09:31:53 -0600

    auth0 setup

--- server/app.js
+++ server/app.js
@@ -2,12 +2,12 @@ import express from 'express';
 import cors from 'cors';
 import testRoute from './routes/test.route.js';
 
-
 const app = express();
 
 app.use(cors({origin: 'http://localhost:5173'}));
-app.use('/api', testRoute);
 app.use(express.json());
 
+// Routes
+app.use('/api', testRoute);
 
 export default app;
